ba78d4e6dbb9c83122ae511882260c39ddff95b1
Qharny <cannycc1020@gmail.com>
2024-11-08 11:07:15 +0000

    deleted everything

--- src/styles/shared.js
+++
@@ -1,5 +0,0 @@
-export const gridPattern = {
-  backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.244) 1px, transparent 1px),
-    linear-gradient(90deg, rgba(255, 255, 255, 0.244) 1px, transparent 1px)`,
-  backgroundSize: '250px 250px'
-} 

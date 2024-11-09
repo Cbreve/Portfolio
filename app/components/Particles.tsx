5308d485adcc277689b5240940404e90cada7516
Qharny <cannycc1020@gmail.com>
2024-11-09 14:36:33 +0000

    new file:   app/components/Particles.tsx 	new file:   app/js/404.js 	new file:   app/js/app.js 	new file:   app/js/particles.min.js

---
+++ app/components/Particles.tsx
@@ -0,0 +1,22 @@
+// 'use client';
+// import React, { useEffect } from 'react';
+
+// const Particles: React.FC = () => {
+//     useEffect(() => {
+//         const particlesJS = require('particles.js');
+
+//         particlesJS.load('particles-js', '/path/to/particles.json', () => {
+//             console.log('callback - particles.js config loaded');
+//         });
+
+//         return () => {
+//             // Cleanup if necessary
+//         };
+//     }, []);
+
+//     return (
+//         <div id="particles-js" className="absolute top-0 left-0 w-full h-full z-0"></div>
+//     );
+// };
+
+// export default Particles;

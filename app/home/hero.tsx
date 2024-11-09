d483bb653bd232940b1431fc9905d78232d835f8
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-11-09 10:01:22 +0000

    feat: updated the hero section

--- app/home/hero.tsx
+++ app/home/hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section className="relative min-h-screen bg-black text-white">
+    <section className="relative min-h-screen max-sm:pt-20 max-lg:pt-16 bg-black text-white">
       <div className="container mx-auto px-4 py-12 md:py-20 flex flex-col items-center justify-center min-h-screen">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center w-full max-w-6xl">
           {/* Text Content */}
@@ -33,33 +33,33 @@ const Hero = () => {
             <img 
               src="https://cdn.prod.website-files.com/667a83531aa35c93f30b287c/668bc631552245e0b1f1f6ac_667d0d3673903a7f735aa1cc_Screenshot%202024-06-27%20at%2010.56.39-p-500.webp"
               alt="Person working" 
-              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-56 object-contain rounded-3xl shadow-lg z-20" 
+              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-56 object-contain rounded-3xl shadow-lg z-20 bg-cover" 
             />
             {/* Icons */}
             <img 
               src="https://cdn.prod.website-files.com/667a83531aa35c93f30b287c/667d218ce38eb57331b53962_Icon_5.svg"
               alt="Icon 1"
-              className="absolute top-1/4 left-1/4 w-6 h-6 md:w-8 md:h-8 z-30"
+              className="absolute top-32 left-6 w-6 h-6 md:w-8 md:h-8 z-30"
             />
             <img 
               src="https://cdn.prod.website-files.com/667a83531aa35c93f30b287c/667d20430bdeb0613eb7260a_Icon_3.svg"
               alt="Icon 2"
-              className="absolute top-1/4 right-1/4 w-6 h-6 md:w-8 md:h-8 z-30"
+              className="absolute top-16 right-16 w-6 h-6 md:w-8 md:h-8 z-30"
             />
             <img 
               src="https://cdn.prod.website-files.com/667a83531aa35c93f30b287c/667d20e58f08a445ae96240d_Icon_4.svg"
               alt="Icon 3"
-              className="absolute bottom-1/4 left-1/4 w-6 h-6 md:w-8 md:h-8 z-30"
+              className="absolute bottom-16 left-16 w-6 h-6 md:w-8 md:h-8 z-30"
             />
             <img 
               src="https://cdn.prod.website-files.com/667a83531aa35c93f30b287c/667d1ea1458bb01af5a47b39_Icon_1.svg"
               alt="Icon 4"
-              className="absolute bottom-1/4 right-1/4 w-6 h-6 md:w-8 md:h-8 z-30"
+              className="absolute bottom-32 right-6 w-6 h-6 md:w-8 md:h-8 z-30"
             />
             <img 
               src="https://cdn.prod.website-files.com/667a83531aa35c93f30b287c/667d1f159491ad7d8138a56e_Icon_2.svg"
               alt="Icon 5"
-              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-6 md:w-8 md:h-8 z-30"
+              className="absolute bottom-0 right-36 transform -translate-x-1/2 -translate-y-1/2 w-6 h-6 md:w-8 md:h-8 z-30"
             />
           </div>
         </div>
